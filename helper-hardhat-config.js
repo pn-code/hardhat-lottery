da6f1c4f45b8b5708b36e6d80ebec02d9636ce2d
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-27 12:41:26 -0700

    Updated helper config to include localhost

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,26 +1,38 @@
-const { ethers } = require("hardhat");
+const { ethers } = require("hardhat")
 
 const networkConfig = {
-    4: {
-        name: "sepolia",
-        vrfCoordinatorV2: "0x447fd5ec2d383091c22b8549cb231a3bad6d3faf",
-        entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane:
-            "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-        subscriptionId: "0",
-        callbackGasLimit: "500000", //500,000
-        interval: 30,
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
     },
     31337: {
-        name: "hardhat",
-        entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane:
-            "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-        callbackGasLimit: "500000", //500,000
-        interval: 30,
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        interval: "30",
+        entranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
     },
-};
+    11155111: {
+        name: "sepolia",
+        subscriptionId: "6926",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        interval: "30",
+        entranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
+    },
+}
 
-const developmentChains = ["hardhat", "localhost"];
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 
-module.exports = { networkConfig, developmentChains };
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+}
