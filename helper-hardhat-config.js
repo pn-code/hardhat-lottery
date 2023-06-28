27cbbe22576cbc0c4ef3001ef753a78f6a3c26d9
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-28 13:54:02 -0700

    Updated gas price for sepolia

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat")
 const networkConfig = {
     default: {
         name: "hardhat",
-        keepersUpdateInterval: "30",
+        interval: "30",
     },
     31337: {
         name: "localhost",
@@ -11,20 +11,20 @@ const networkConfig = {
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
         interval: "30",
         entranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
-        callbackGasLimit: "500000", // 500,000 gas
+        callbackGasLimit: "700000", // 500,000 gas
     },
     11155111: {
         name: "sepolia",
-        subscriptionId: "6926",
+        subscriptionId: "3063",
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
         interval: "30",
         entranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
-        callbackGasLimit: "500000", // 500,000 gas
+        callbackGasLimit: "700000", // 500,000 gas
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
     },
     1: {
         name: "mainnet",
-        keepersUpdateInterval: "30",
+        interval: "30",
     },
 }
 
