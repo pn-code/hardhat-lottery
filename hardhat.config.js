26d848006feaa7bfedd9e3f3b9bd5316cc0fffff
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-28 13:53:04 -0700

    Cleaned up comments & added Etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,17 +13,14 @@ require("dotenv").config();
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-const REPORT_GAS = process.env.REPORT_GAS;
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const REPORT_GAS = process.env.REPORT_GAS || false;
 
 module.exports = {
-    solidity: "0.8.18",
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
             chainId: 31337,
         },
         localhost: {
@@ -32,18 +29,32 @@ module.exports = {
         sepolia: {
             url: SEPOLIA_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
             saveDeployments: true,
             chainId: 11155111,
         },
+    },
+    etherscan: {
+        // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "sepolia",
+                chainId: 11155111,
+                urls: {
+                    apiURL: "https://api-sepolia.etherscan.io/api",
+                    browserURL: "https://sepolia.etherscan.io",
+                },
+            },
+        ],
     },
     gasReporter: {
         enabled: REPORT_GAS,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
     contractSizer: {
         runOnCompile: false,
@@ -58,6 +69,7 @@ module.exports = {
             default: 1,
         },
     },
+    solidity: "0.8.18",
     mocha: {
         timeout: 500000, // 500 seconds max for running tests
     },
